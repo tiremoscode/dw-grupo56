87abd4d382055763595f7d4ca52b48c9724bf2b2
Alyn Lizeth Sanchez Diaz <kilizeth.aly.alen@gmail.com>
2024-10-13 16:32:41 -0600

    feature/calculadora-alynsanchez

---
+++ practicas/calculadoraJS/calcuJSAlynSanchez/calculadora_alyns.js
@@ -0,0 +1,61 @@
+
+function sumar() {
+    const valor1 = parseInt(document.getElementById('numero1').value);
+    const valor2 = parseInt(document.getElementById('numero2').value);
+
+    console.log('Valor 1 ' + valor1);
+    console.log('valor 2 ' + valor2);
+
+    const suma = valor1 + valor2;
+    console.log(suma);
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Resultado: ${suma}`;
+
+}
+
+function resta() {
+    const valor1 = parseInt(document.getElementById('numero1').value);
+    const valor2 = parseInt(document.getElementById('numero2').value);
+
+    console.log('Valor 1 ' - valor1);
+    console.log('valor 2 ' - valor2);
+
+    const resta = valor1 - valor2;
+    console.log(resta);
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Resultado: ${resta}`;
+
+}
+
+
+function multiplicar() {
+    const valor1 = parseInt(document.getElementById('numero1').value);
+    const valor2 = parseInt(document.getElementById('numero2').value);
+
+    console.log('Valor 1 ' * valor1);
+    console.log('valor 2 ' * valor2);
+
+    const multiplicar = valor1 * valor2;
+    console.log(multiplicar);
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Resultado: ${multiplicar}`;
+
+}
+
+function dividir() {
+    const valor1 = parseInt(document.getElementById('numero1').value);
+    const valor2 = parseInt(document.getElementById('numero2').value);
+
+    console.log('Valor 1 ' / valor1);
+    console.log('valor 2 ' / valor2);
+
+    const dividir = valor1 / valor2;
+    console.log(dividir);
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Resultado: ${dividir}`;
+
+}
