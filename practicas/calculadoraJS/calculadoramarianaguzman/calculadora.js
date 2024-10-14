8c25da4324de361874ee50c4e57de8c06d097dee
Mar <lamarvaltierra@gmail.com>
2024-10-13 18:49:47 -0600

    Segundo cambio calcu

--- practicas/calculadoraJS/calculadoramarianaguzman/calculadora.js
+++ practicas/calculadoraJS/calculadoramarianaguzman/calculadora.js
@@ -1,13 +1,15 @@
-.calculadora {
-    background-color: #003366;
-    display: flex;
-    flex-direction: column;
-}
 
-.datos {
-    background-color: green;
-}
+function sumar() {
+    const valor1 = parseInt(document.getElementById('numero1').value);
+    const valor2 = parseInt(document.getElementById('numero2').value);
 
-.resultado {
-    background-color: yellow;
+    console.log('Valor 1 ' + valor1);
+    console.log('valor 2 ' + valor2);
+
+    const suma = valor1 + valor2;
+    console.log(suma);
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Resultado: ${suma}`;
+
 }
