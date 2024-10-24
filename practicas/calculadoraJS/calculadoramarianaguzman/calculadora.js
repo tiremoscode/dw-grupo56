5a1dd5103fce60ea96cd230b6ef4af357d18a986
Mar <lamarvaltierra@gmail.com>
2024-10-24 00:43:03 -0600

    subo calculadora

--- practicas/calculadoraJS/calculadoramarianaguzman/calculadora.js
+++ practicas/calculadoraJS/calculadoramarianaguzman/calculadora.js
@@ -11,5 +11,33 @@ function sumar() {
 
     const resultado = document.getElementById('resultado');
     resultado.innerHTML = `Resultado: ${suma}`;
-    resultado.innerHTML = `Resultado2: ${resta}`;
+
+
 }
+function restar() {
+    const valor1 = parseInt(document.getElementById('numero1').value);
+    const valor2 = parseInt(document.getElementById('numero2').value);
+
+    console.log('Valor 1 ' + valor1);
+    console.log('valor 2 ' + valor2);
+
+    const resta = valor1 - valor2;
+    console.log(resta);
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `Resultado: ${resta}`;
+}
+
+function multiplicar() {
+    const valor1 = parseInt(document.getElementById('numero1').value);
+    const valor2 = parseInt(document.getElementById('numero2').value);
+
+    console.log('Valor 1 ' + valor1);
+    console.log('valor 2 ' + valor2);
+
+    const multiplicanding = valor1 * valor2;
+    console.log(multiplicanding);
+    const resultado = document.getElementById ('resultado');
+    resultado.innerHTML = `Resultado: ${multiplicanding}`;
+}
+
