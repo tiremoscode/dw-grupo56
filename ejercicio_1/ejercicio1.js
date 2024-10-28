3b05b4c8588815e0a5e91c2245fd3533d15a49d8
Avryl <avolvera109@gmail.com>
2024-10-28 11:05:44 -0600

    agregar ejercicios actualizados

--- ejercicio_1/ejercicio1.js
+++ ejercicio_1/ejercicio1.js
@@ -1,8 +1,4 @@
-// Utilizando Document Write
 
-// document.write("Hola Mundo - Grupo 56");
-
-// Utilizando Inner HTML
 const mensaje = document.getElementById("holaMundo");
 
 mensaje.innerHTML = "<h1>Hola Mundo</h1> - <p>Grupo 56</p>";
