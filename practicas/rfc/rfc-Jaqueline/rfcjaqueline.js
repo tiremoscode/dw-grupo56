d59e11a287980452a0410d7993a8853999377266
jaquelie <yake.alamilla.2302@gmail.com>
2024-10-28 09:10:53 -0600

    mirfc

---
+++ practicas/rfc/rfc-Jaqueline/rfcjaqueline.js
@@ -0,0 +1,44 @@
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('¿Cuál es tu nombre?', (name) => {
+    name = name.substring(0, 1)
+    rl.question('¿Cuál es tu apellido paterno?', (apellidoPaterno) => {
+        apellidoPaterno = apellidoPaterno.substring(0,2)
+        rl.question('¿Cuál es tu apellido materno?', (apellidoMaterno) => {
+            apellidoMaterno = apellidoMaterno.substring(0, 1)
+           rl.question('¿Cuál es tu Día de Nacimiento (2 dígitos)? ', (diaNac) => { 
+                
+                rl.question('¿Cuál es tu Mes de Nacimiento (2 dígitos)? ', (mesNac) => {
+                   
+                    
+                    rl.question('¿Cuál es tu Año de Nacimiento (2 dígitos)? ', (anioNac) => {
+
+                         console.log(`Obtendras tu RFC: ${name + apellidoPaterno +apellidoMaterno +diaNac+mesNac+anioNac+ "xxx"}`)
+                   
+
+
+
+                         rl.close();
+
+              
+
+                
+ 
+                    });
+                });
+  
+            });
+
+        });
+   });
+
+    
+
+    
+});
+
+
+
