0d039020a62d0db50e6e1a225f4b2212de6dfd3e
Sandra Salazar <sandrasalazaarh@gmail.com>
2024-10-28 11:59:48 -0600

    mi rfc por no se que numero de vez

---
+++ practicas/rfc/rfc-sandrasalazar/rfcsandrasalazar.js
@@ -0,0 +1,32 @@
+const readline = require('readline');
+function holaMundo() {
+    console.log('Hola Grupo 56 desde consola');
+}
+
+holaMundo();
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+rl.question('¿Cuál es tu nombre?', (name) => {
+    name = name.substring(0, 1)
+    rl.question('¿Cuál es tu apellido paterno?', (paterno) => {
+        paterno = paterno.substring(0, 2)
+        rl.question('¿Cuál es tu apellid materno?', (materno) => {
+            materno = materno.substring(0, 1)
+            rl.question('¿Cuál es tu día de nacimiento?', (día) => {
+                rl.question('¿Cuál es tu mes de nacimiento?', (mes) => {
+                    rl.question('¿Cuál es tu anio de nacimiento?', (anio) => {
+                        console.log( name +paterno + materno + día + mes + anio + "xxx")
+                    })
+                })
+
+            })
+
+        })
+
+    })
+});
