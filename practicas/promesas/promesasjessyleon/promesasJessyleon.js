29608a615c4955b1546b9b114b582e9555802016
jessy leon <jessyca_Ariadna@hotmail.com>
2024-10-28 19:46:38 -0600

    promesas de jessyleon

---
+++ practicas/promesas/promesasjessyleon/promesasJessyleon.js
@@ -0,0 +1,20 @@
+
+
+function obtenerPersonaje() {
+    const url = 'https://www.breakingbadapi.com/api/';
+
+    fetch(url)
+        .then(respuesta => respuesta.json())
+        .then(data => {
+            for (let i = 0; i < data.results.length; i++) {
+                console.log('Nombre de maton' + data.results[i].name);
+            }
+        })
+        .catch(error => {
+            console.log('Hubo un error', error);
+        });
+};
+obtenerPersonaje();
+
+
+
