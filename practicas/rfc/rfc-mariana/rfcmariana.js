334c693a902f5b55f3ab8a4ea8e8d1747de68643
Mar <lamarvaltierra@gmail.com>
2024-11-06 17:03:09 -0600

    hola 2

--- practicas/rfc/rfc-mariana/rfcmariana.js
+++ practicas/rfc/rfc-mariana/rfcmariana.js
@@ -1,10 +1,23 @@
+const { normalize } = require('path');
 const readline = require('readline');
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
+//Preguntar al usuario si quiere obtener rfc
 
 rl.question ('¿Quieres obtener tu rfc?'), (respuesta)=> {
-si= continuar
+if (respuesta = si) {
+    console.log ('De acuerdo');
+}
+else  {
+    console.log ('Vuelve pronto')
+}
 };
+
+var generarRfc = true;
+if (respuesta == si){
+rl.question ('¿Cual es tu nombre?'),(nombre) =
+
+}
