f35b386efbc759de48d2de8758474b10be0c93ad
Mar <lamarvaltierra@gmail.com>
2024-11-05 20:31:09 -0600

    primer cambio rfc

---
+++ practicas/rfc/rfc-mariana/rfcmariana.js
@@ -0,0 +1,10 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question ('¿Quieres obtener tu rfc?'), (respuesta)=> {
+si= continuar
+};
