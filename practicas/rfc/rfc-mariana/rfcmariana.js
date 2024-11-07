33c3cf5fb6840770cfb951aa739876280d554e88
Mar <lamarvaltierra@gmail.com>
2024-11-06 18:06:06 -0600

    por favor revise mi codigo

--- practicas/rfc/rfc-mariana/rfcmariana.js
+++ practicas/rfc/rfc-mariana/rfcmariana.js
@@ -16,8 +16,31 @@ else  {
 }
 };
 
-var generarRfc = true;
-if (respuesta == si){
-rl.question ('¿Cual es tu nombre?'),(nombre) =
+rl.question ('¿Cuál es tu nombre?'), (nombre) => {
+    nombre = nombre.substring(0,1)
 
+    rl.question ('¿Cuál es tu apellido materno?'), (amaterno)=> {
+        amaterno = amaterno.substring (0,1)
+
+        rl.question ('¿Cuál es tu apellido paterno'), (apaterno)=> {
+            apaterno = apaterno.substring (0,2)
+
+            rl.question ('Cuál es tu fecha de nacimiento? (DD/MM/AA)'), (nacimiento)=> {
+            rl.close ();
+
+            let rfc = ((apaterno.toUpperCase())+ (amaterno.toUpperCase()+ (nombre.toUpperCase())+ (nacimiento.substring(6,8)+ (nacimiento.substring(3,5))+ (nacimiento(0,2)+ XXX); )
+    
+            console.log('Tu RFC es:'+ rfc)
+            }
+
+        } 
+    } 
 }
+// var generarRfc = true;
+// if (respuesta == si){
+// rl.question ('¿Cual es tu nombre?'),(nombre) =
+
+
+
+
+// Pedir datos de RFC
